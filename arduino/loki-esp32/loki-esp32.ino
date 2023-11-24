3846fc232539dc5aab335bf053588380802e311e
Ilia O. <iliao@makerspet.com>
2023-11-24 00:53:33 -0800

    Library version check

--- arduino/loki-esp32/loki-esp32.ino
+++ arduino/loki-esp32/loki-esp32.ino
@@ -1,3 +1,17 @@
+// Copyright 2023 REMAKE AI
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "robot_config.h"
 #include "util.h"
 #include <WiFi.h>
@@ -17,6 +31,10 @@
 #include <rclc_parameter/rclc_parameter.h>
 #include "drive.h"
 #include "ap.h"
+
+#if !defined(IS_MICRO_ROS_KAIA_MIN_VERSION) || !IS_MICRO_ROS_KAIA_MIN_VERSION(2,0,7,3)
+#error "Please upgrade micro_ros_kaia library version"
+#endif
 
 #define RCCHECK(fn,E) { rcl_ret_t temp_rc = fn; \
   if((temp_rc != RCL_RET_OK)){error_loop((E));}}
