0f045dee8a4f86effee7fea3f655120e0cdec275
Ilia O. <iliao@makerspet.com>
2023-12-25 23:06:50 -0800

    Pico servo, ADC works

---
+++ arduino/loki-pico-head/robot_config.h
@@ -0,0 +1,16 @@
+#ifndef ROBOT_CONFIG
+#define ROBOT_CONFIG
+
+#define I2C_SDA_HOST_PIN  0
+#define I2C_SCL_HOST_PIN  1
+#define INT_HOST          2
+
+#define TOUCH_TOP         6
+#define TOUCH_LEFT        7
+#define TOUCH_RIGHT       8
+#define TOUCH_REAR_PIN    9
+
+#define FAN_EN_PIN       10
+#define ADC3_PIN         29 // measures VSYS/3
+
+#endif  // ROBOT_CONFIG
