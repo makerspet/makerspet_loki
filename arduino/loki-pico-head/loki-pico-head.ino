db8219a0355d08abba8838ef467495e859182c79
Ilia O. <iliao@makerspet.com>
2023-12-25 23:06:50 -0800

    Pico servo, ADC works

---
+++ arduino/loki-pico-head/loki-pico-head.ino
@@ -0,0 +1,74 @@
+// https://deepbluembedded.com/arduino-i2c-slave/
+// https://arduino-pico.readthedocs.io/en/latest/wire.html
+// https://randomnerdtutorials.com/programming-raspberry-pi-pico-w-arduino-ide/
+// https://github.com/earlephilhower/arduino-pico/tree/master/libraries/Servo/src
+#include <Wire.h>
+#include <Servo.h>
+#include "robot_config.h"
+
+Servo leftServo;
+Servo rightServo;
+Servo extServo;
+Servo fan;
+
+void I2C_RxHandler(int numBytes) {
+  while(Wire.available()) {  // Read Any Received Data
+    byte RxByte = Wire.read();
+    Serial.print(RxByte);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  delay(2000);
+  
+  Serial.println("Start setup()");
+  if (!Wire.setSDA(I2C_SDA_HOST_PIN))
+    Serial.println("SDA pin config failed");
+  if (!Wire.setSCL(I2C_SCL_HOST_PIN))
+    Serial.println("SCL pin config failed");
+  
+  Wire.begin(0x55); // Initialize I2C (Slave Mode: address=0x55 )
+  Wire.onReceive(I2C_RxHandler);
+
+  leftServo.attach(13);
+  rightServo.attach(14);
+  extServo.attach(15);
+  analogReadResolution(12);
+
+  Serial.println("Done setup()");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  leftServo.write(0);
+//  rightServo.write(90);
+//  extServo.write(90);
+    delay(500);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  int base = analogRead(ADC3_PIN);
+  Serial.print(base);
+  Serial.print(" ");
+
+  leftServo.write(25);
+
+  for (int i = 0; i < 500; i++) {
+    int v = analogRead(ADC3_PIN);
+    int delta = v - base;
+    if (abs(delta) > 7) {
+      Serial.print(i);
+      Serial.print(":");
+      Serial.print(delta);
+      Serial.print(" ");
+    }
+    delay(1);
+  }
+  Serial.println();
+
+//float analogReadTemp(float vref = 3.3f)
+}
